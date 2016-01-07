544338f28555bb1d20347a364bfaec9dc7d74534
Alexander Smith <ASmith@predictivetechnologies.com>
2016-01-07 18:03:44 -0500

    Refactoring some of the cshtml / js / css files

---
+++ src/HNGRY/wwwroot/js/foodFeed.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+	var
+
+	initialize = function() {
+		// Put the initialization stuff here
+	},
+
+	renderUI = function() {
+		// Put any initial rendering stuff here
+	},
+
+	bindUI = function() {
+		// Put any bindings here
+	},
+	_done;
+
+	initialize();
+	renderUI();
+	bindUI();
+});
