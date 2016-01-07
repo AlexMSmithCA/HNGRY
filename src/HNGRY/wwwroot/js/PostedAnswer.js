20506b0dd30c5bb26e6fdca14b89669767b0a862
Brad Stankey <bstankey@predictivetechnologies.com>
2016-01-07 16:54:29 -0500

    we adding more submissions

--- src/HNGRY/wwwroot/js/PostedAnswer.js
+++ src/HNGRY/wwwroot/js/PostedAnswer.js
@@ -1,41 +1,46 @@
-﻿$(document).ready(function() {
-	var SUBMIT_QUESTION_TEXT_CSS = ".submit-question-text",
+﻿$(document).ready(function () {
+    var
+		/* SELECTORS */
+		SUBMIT_QUESTION_TEXT_CSS = ".submit-question-text",
 		SUBMIT_QUESTION_BUTTON_CSS = ".submit-question-button",
 		SUBMIT_QUESTION_URL = "/Home/SubmitQuestion",
+
+		/* CONSTANTS */
 		CLICK = "click",
+		POST = "POST",
 
-	initialize = function() {
-		// Put the initialization stuff here
+	initialize = function () {
+	    // Put the initialization stuff here
 	},
 
-	renderUI = function() {
-		// Put any initial rendering stuff here
+	renderUI = function () {
+	    // Put any initial rendering stuff here
 	},
 
-	bindUI = function() {
-		// Put any bindings here
-		$(SUBMIT_QUESTION_BUTTON_CSS).on(CLICK, function(event) {
-			var questionToSubmit = $(SUBMIT_QUESTION_TEXT_CSS).text();
-			$.ajax({
-					method: "POST",
-					url: SUBMIT_QUESTION_URL,
-					data: { text: questionToSubmit }
-				})
-				.done(function() {
-					console.log("Your question was submitted.");
-				})
-				.fail(function() {
-					console.log("Yikes!  Something went wrong :(.  Please contact Sean Walsh for assistance.");
-				});
-		});
+	bindUI = function () {
+	    // Put any bindings here
+	    $(SUBMIT_QUESTION_BUTTON_CSS).on(CLICK, function (event) {
+	        _submitQuestion();
+	    });
 	},
 
-	_submitQuestion = function() {
-		
-	}
+	_submitQuestion = function () {
+	    var questionToSubmit = $(SUBMIT_QUESTION_TEXT_CSS).val();
+	    $.ajax({
+	        method: POST,
+	        url: SUBMIT_QUESTION_URL,
+	        data: { text: questionToSubmit }
+	    })
+			.done(function () {
+			    console.log("Your question was submitted.");
+			})
+			.fail(function () {
+			    console.log("Yikes!  Something went wrong :(.  Please contact Sean Walsh for assistance.");
+			});
+	},
+	_done;
 
-	initialize();
-	renderUI();
-	bindUI();
-
+    initialize();
+    renderUI();
+    bindUI();
 });
