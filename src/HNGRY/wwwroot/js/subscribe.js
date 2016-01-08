b54761a1ceeecf46fdd6187c2ca9bc2a6369925a
Sean Walsh <SWalsh@predictivetechnologies.com>
2016-01-07 23:07:05 -0500

    Updated subscribe

--- src/HNGRY/wwwroot/js/subscribe.js
+++ src/HNGRY/wwwroot/js/subscribe.js
@@ -3,20 +3,24 @@
 		/* SELECTORS */
         SUBSCRIBE_EMAIL_CSS = "#inputEmail3",
         SUBSCRIBE_PHONE_CSS = "#inputPhoneNumber3",
+        SUBSCRIBE_EMAIL_DIV_CSS = ".email-input",
+        SUBSCRIBE_PHONE_DIV_CSS = ".phone-input",
 		SUBSCRIBE_BUTTON_CSS = ".subscribe-button",
         FOOD_CHECKBOX_CSS = "#foodSubmissionCheckbox",
         POSTS_CHECKBOX_CSS = "#postsFromCheckbox",
         EMAIL_CHECKBOX_CSS = "#emailCheckbox",
         PHONE_CHECKBOX_CSS = "#phoneCheckbox",
-        SUBSCRIBE_SUCCESS_CSS = ".subscribe-success",
+        SUBSCRIBE_SUCCESS_CSS = ".success-message",
         SUBSCRIBE_URL = "/Data/Subscribe",
+
 
 		/* CONSTANTS */
 		CLICK = "click",
 		POST = "POST",
+        CHECKED = ":checked",
 
 	initialize = function () {
-	    // Put the initialization stuff here
+	    
 	},
 
 	renderUI = function () {
@@ -24,6 +28,23 @@
 	},
 
 	bindUI = function () {
+	    
+	    $(EMAIL_CHECKBOX_CSS).on(CLICK, function (event) {
+	        if ($(EMAIL_CHECKBOX_CSS).is(CHECKED)) {
+	            $(SUBSCRIBE_EMAIL_DIV_CSS).show();
+	        } else {
+	            $(SUBSCRIBE_EMAIL_DIV_CSS).hide();
+	        }
+	    });
+	    $(PHONE_CHECKBOX_CSS).on(CLICK, function (event) {
+	        if ($(PHONE_CHECKBOX_CSS).is(CHECKED)) {
+	            $(SUBSCRIBE_PHONE_DIV_CSS).show();
+	        } else {
+	            $(SUBSCRIBE_PHONE_DIV_CSS).hide();
+	        }
+	    });
+
+
 	    // Put any bindings here
 	    $(SUBSCRIBE_BUTTON_CSS).on(CLICK, function (event) {
 	        _subscribe();
@@ -33,10 +54,10 @@
 	_subscribe = function () {
 	    var emailToSubmit = $(SUBSCRIBE_EMAIL_CSS).val();
 	    var phoneToSubmit = $(SUBSCRIBE_PHONE_CSS).val();
-	    var foodCheckboxToSubmit = + $(FOOD_CHECKBOX_CSS).is(':checked');
-	    var postsCheckboxToSubmit = + $(POSTS_CHECKBOX_CSS).is(':checked');
-	    var emailCheckboxToSubmit = + $(EMAIL_CHECKBOX_CSS).is(':checked');
-	    var phoneCheckboxToSubmit = + $(PHONE_CHECKBOX_CSS).is(':checked');
+	    var foodCheckboxToSubmit = + $(FOOD_CHECKBOX_CSS).is(CHECKED);
+	    var postsCheckboxToSubmit = + $(POSTS_CHECKBOX_CSS).is(CHECKED);
+	    var emailCheckboxToSubmit = + $(EMAIL_CHECKBOX_CSS).is(CHECKED);
+	    var phoneCheckboxToSubmit = + $(PHONE_CHECKBOX_CSS).is(CHECKED);
 
 	    $.ajax({
 	        method: POST,
