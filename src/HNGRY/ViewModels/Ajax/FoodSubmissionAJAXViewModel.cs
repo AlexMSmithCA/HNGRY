6f018a5dd34c629bd9c7e154e25f808d3993e0c3
Kevin Ngo <kngo@arlington.PredictiveTechnologies.com>
2016-01-07 19:28:03 -0500

    We can add food

---
+++ src/HNGRY/ViewModels/Ajax/FoodSubmissionAJAXViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HNGRY.ViewModels.Ajax
+{
+    public class FoodSubmissionAjaxViewModel
+    {
+        public string Location { get; set; }
+        public string Message { get; set; }
+    }
+}
