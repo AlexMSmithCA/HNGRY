d4215ff8f1e6287a106d0c9255b18d6a3daebb84
Sean Walsh <SWalsh@predictivetechnologies.com>
2016-01-08 11:00:28 -0500

    Updated seed

--- src/HNGRY/SampleSeeder/SeedFeedEntries.cs
+++ src/HNGRY/SampleSeeder/SeedFeedEntries.cs
@@ -18,10 +18,12 @@
 			var users = this._appRepository.GetUsers();
 			if (!this._appRepository.GetFeedEntries().Any() && users.Count >= 2)
 			{
-				await this._appRepository.AddFoodSubmission(users[0].Id, "10th Floor", "Clam chowder");
-				await this._appRepository.AddFoodSubmission(users[1].Id, "11th Floor", "Tomato paste");
-				await this._appRepository.AddFoodSubmission(users[0].Id, "9th Floor", "Chopped Panda");
-			}
+				await this._appRepository.AddFoodSubmission(users[0].Id, "10th Floor", "Uncle Julios is here!");
+				await this._appRepository.AddFoodSubmission(users[1].Id, "11th Floor", "Leftover chips and guac, #snackathon");
+				await this._appRepository.AddFoodSubmission(users[0].Id, "10th Floor", "Pizza on 10, the veggie is so good :)");
+                await this._appRepository.AddFoodSubmission(users[1].Id, "10th Floor", "Omma'let you all know there's omlettes on 10, enjoy!");
+                await this._appRepository.AddFoodSubmission(users[0].Id, "9th Floor", "Try my homemade Chobani, because there's never enough in the fridge.");
+            }
 		}
 	}
 }
